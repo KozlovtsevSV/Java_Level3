c08f40914d3eed23eda416115ade7d984d81820e
Sergey <KozlovtsevSV@gmail.com>
2021-01-19 19:34:47 +0300

    HW#1

---
+++ HW/lesson_1/Apple.java
@@ -0,0 +1,8 @@
+package ru.geekbrains;
+
+public class Apple extends Fruit {
+    Apple(){
+        weightFruit = 1.0f;
+    }
+
+}
---
+++ HW/lesson_1/Fruit.java
@@ -0,0 +1,10 @@
+package ru.geekbrains;
+
+public class Fruit {
+    public float weightFruit;
+
+
+    public float weightFruit() {
+        return weightFruit;
+    }
+}
