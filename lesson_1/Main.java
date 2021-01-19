fab366c1a1c28e85db46ae1d1c32496b71ae0eb3
Sergey <KozlovtsevSV@gmail.com>
2021-01-19 19:31:00 +0300

    f

--- lesson_1/Main.java
+++ lesson_1/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         System.out.println("============ ЗАДАНИЕ №3 ============");
 
-        class Box<T> {
+        class Box<T> extends Fruit {
 
             private ArrayList<T> items = new ArrayList<>();
             private float maxWeigh = 12f;
